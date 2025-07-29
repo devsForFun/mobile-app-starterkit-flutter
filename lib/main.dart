709ee5aa1698aeb23e2bf6a97ce51cfd71ddf0b9
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-07-29 19:31:02 +0530

    Refactor main application entry point to initialize environment variables and Supabase, and update app structure to use GetMaterialApp with LoginScreen as the home widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/data/repositories/authentication/authentication_repo.dart';
+import 'package:flutter_template/features/authentication/screens/login/login.dart';
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'utils/theme/theme.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  ).then((Supabase value) => Get.put(AuthenticationRepository()));
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: KAppTheme.lightTheme,
+      darkTheme: KAppTheme.darkTheme,
+      home: const LoginScreen(),
     );
   }
 }
