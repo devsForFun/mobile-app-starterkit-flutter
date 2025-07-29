9c9c66164bcc925d6ef659cf1d73df170d1c649b
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-07-29 19:27:03 +0530

    Add shadow and spacing style classes for consistent UI design

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/utils/constants/sizes.dart';
+
+class KSpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: KSizes.appBarHeight,
+    left: KSizes.defaultSpace,
+    bottom: KSizes.defaultSpace,
+    right: KSizes.defaultSpace,
+  );
+}
