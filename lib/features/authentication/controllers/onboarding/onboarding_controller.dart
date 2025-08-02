1f71a27c7247623239639ed7bf93d0a743eaa9e2
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-08-02 11:39:12 +0530

    Add OnBoardingController to manage onboarding page navigation and user flow

---
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import '../../../../features/authentication/screens/login/login.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  //Variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  //Update Current Index when Page Scroll
+  void updatePageIndicator(int index) => currentPageIndex.value = index;
+
+  //Jump to the specific dot selected page
+  void dotNavigationClick(int index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index.toDouble());
+  }
+
+  //Update Current index and jump to next page
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      final storage = GetStorage();
+      storage.write('isFirstTime', false);
+      Get.to(const LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  //Update Current Index and jump to the last page
+  void skipPage() {
+    Get.to(const LoginScreen());
+  }
+}
